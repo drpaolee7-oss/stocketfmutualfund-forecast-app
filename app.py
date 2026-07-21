fecf55c387c843cd0e03710a9cfd44f84dd205e2
drpaolee7-oss <drpaolee7@gmail.com>
2026-07-21 19:59:10 -0400

    Add new tickers to the watchlist default value

--- app.py
+++ app.py
@@ -235,7 +235,7 @@ with st.sidebar:
     watch_raw = st.text_area(
         "Comma-separated tickers",
         #value=st.session_state.get("watchlist", "BWET, APLZ, DUST, AMA, KLAG, SPY, SPYM, QQQM, QQQ, SCHD, VEA, SCHF, VXUS, IXUS, IEFA, VOO, VDC"),
-        value=st.session_state.get("watchlist", "BWET, SWTSX, FZROX, VTSAX, DIA, VYM, SDIV, DIV, SCHD, SPYD"),
+        value=st.session_state.get("watchlist", "BWET, SWTSX, FZROX, VTSAX, DIA, VYM, SDIV, DIV, SCHD, SPYD, JEPI, KBWY, SPHY, USHY, HYG, PFF"),
         height=80,
         label_visibility="collapsed",
     )
